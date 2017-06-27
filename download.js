3131f623fc9d0b43b28f66b0a1fa9bdc45f59a44
Yusuke Ando <yandod@gmail.com>
2017-06-27 21:57:31 +0900

    done download

--- download.js
+++ download.js
@@ -2,18 +2,35 @@ var csv = require('csv');
 var fs = require('fs');
 var parse = require('csv-parse');
 var execSync = require('child_process').execSync;
+var opts = require('opts');
+
+opts.parse([
+    {
+        'short': 'i',
+        'long': 'index',
+        'description': 'index for starting point of download',
+        'value': true,
+        'required': false
+    },
+]);
+
+var index = opts.get('index') || 0;
 
 //child_process.execSync関数を利用する
 //var result = "" + execSync('cat *.js bad_file | wc -l');
 
 var parser = parse({delimiter: ','}, function(err, data){
   //var result = "" + execSync('cat *.js bad_file | wc -l');
-  data.forEach(function(row){
+  data.slice(index).forEach(function(row){
     if (row[6] == 'なし') {
       return;
     }
-    var result = execSync('wget -r -L -P ./temp/' + row[0] + " " + row[6]);
-    console.log(result);
+    if (row[6].startsWith('https://www.facebook.com', 0)) {
+      return;
+    }
+    console.log("downloading " + row[6]);
+    var result = execSync('wget -q -r -L -T 60 -nc -w 1 -P ./temp/' + row[0] + " " + row[6]);
+    //console.log(result);
   });
 });
 
